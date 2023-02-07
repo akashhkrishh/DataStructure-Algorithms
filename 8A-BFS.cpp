bd23ea36fd4de5a25c19a34c58eafb1087ccb800
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-02-07 22:11:02 +0530

    Update 8A-BFS.cpp

--- 8A-BFS.cpp
+++ 8A-BFS.cpp
@@ -1,5 +1,3 @@
-// BFS algorithm in C++
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -15,20 +13,19 @@ class Graph {
   void BFS(int startVertex);
 };
 
-// Create a graph with given vertices,
-// and maintain an adjacency list
+
 Graph::Graph(int vertices) {
   numVertices = vertices;
   adjLists = new list<int>[vertices];
 }
 
-// Add edges to the graph
+
 void Graph::addEdge(int src, int dest) {
   adjLists[src].push_back(dest);
   adjLists[dest].push_back(src);
 }
 
-// BFS algorithm
+
 void Graph::BFS(int startVertex) {
   visited = new bool[numVertices];
   for (int i = 0; i < numVertices; i++)
